d12c3e222cba03828436f00f15b70447cbc86817
Jim Moffitt <jimmoffitt@yahoo.com>
2016-07-01 13:27:58 -0600

    Update jsondiffer.rb

--- lib/jsondiffer.rb
+++ lib/jsondiffer.rb
@@ -6,93 +6,109 @@ class JsonDiffer
 	  !!obj == obj
    end
 
-   def compare_elements(old, new)
+   def numeric?(string)
+	  Float(string) != nil rescue false
+   end
+
+   def compare_elements(first, second)
 	  diff = {}
-	  if old.kind_of? Hash
-		 if new.kind_of? Array
-			diff_hash = compare_hash_array(old, new)
+	  if first.kind_of? Hash
+		 if second.kind_of? Array
+			diff_hash = compare_hash_array(first, second)
 		 else
-			diff_hash = compare_hashes(old, new)
+			diff_hash = compare_hashes(first, second)
 		 end
 		 diff = diff_hash if diff_hash.count > 0
-	  elsif (!is_boolean(old) || !is_boolean(new)) && old.class != new.class
-		 diff = new
-	  elsif old.kind_of? Array
-		 diff_arr = compare_arrays(old, new)
+	  elsif (!is_boolean(first) || !is_boolean(second)) && first.class != second.class
+		 diff = second
+	  elsif first.kind_of? Array
+		 diff_arr = compare_arrays(first, second)
 		 diff = diff_arr if diff_arr.count > 0
 	  else
-		 string_diff = compare_strings(old, new)
-		 diff = string_diff unless string_diff.nil?
+		 if numeric?(first) and numeric?(second)
+
+			diff = Float(second) - Float(first)
+
+		 else
+			string_diff = compare_strings(first, second)
+			diff = string_diff unless string_diff.nil?
+		 end
+
 	  end
 	  diff
    end
 
-   def compare_hashes(old_hash, new_hash)
-	  old_hash = old_hash == nil ? {} : old_hash
-	  new_hash = new_hash == nil ? {} : new_hash
-	  keys = (old_hash.keys + new_hash.keys).uniq
+   def compare_hashes(first_hash, second_hash)
+	  first_hash = first_hash == nil ? {} : first_hash
+	  second_hash = second_hash == nil ? {} : second_hash
+	  keys = (first_hash.keys + second_hash.keys).uniq
 	  result = get_diffs_struct
 	  keys.each do |k|
-		 if !old_hash.has_key? k
-			result[:append][k] = new_hash[k]
-		 elsif !new_hash.has_key? k
-			result[:remove][k] = old_hash[k]
+		 if !first_hash.has_key? k
+			result[:append][k] = second_hash[k]
+		 elsif !second_hash.has_key? k
+			result[:remove][k] = first_hash[k]
 		 else
-			diff = compare_elements(old_hash[k], new_hash[k])
+			diff = compare_elements(first_hash[k], second_hash[k])
 			if diff.respond_to? "empty?"
 			   result[:update][k] = diff unless diff.empty?
 			else
-			   result[:update][k] = diff # some classes have no empty? method
+
+			   if diff != 0.0
+				  result[:update][k] = diff # some classes have no empty? method
+			   else
+				  result[:same][k] = diff # some classes have no empty? method
+			   end
 			end
 		 end
 	  end
 	  filter_results(result)
    end
 
-   def compare_arrays(old_array, new_array)
-	  old_array_length = old_array.count
-	  new_array_length = new_array.count
-	  inters = [old_array.count, new_array.count].min
+   def compare_arrays(first_array, second_array)
+	  old_array_length = first_array.count
+	  new_array_length = second_array.count
+	  inters = [first_array.count, second_array.count].min
 
 	  result = get_diffs_struct
 
 	  (0..inters).map do |n|
-		 res = compare_elements(old_array[n], new_array[n])
+		 res = compare_elements(first_array[n], second_array[n])
 		 result[:update][n] = res unless (res.nil? || (res.respond_to?(:empty?) && res.empty?))
 	  end
 
 	  # the rest of the larger array
 	  if inters == old_array_length
 		 (inters..new_array_length).each do |n|
-			result[:append][n] = new_array[n]
+			result[:append][n] = second_array[n]
 		 end
 	  else
 		 (inters..old_array_length).each do |n|
-			result[:remove][n] = old_array[n]
+			result[:remove][n] = first_array[n]
 		 end
 	  end
 
 	  filter_results(result)
    end
 
-   def compare_hash_array(old_hash, new_array)
+   def compare_hash_array(first_hash, second_array)
 	  result = get_diffs_struct
 
-	  (0..new_array.count).map do |n|
-		 next if new_array[n].nil?
+	  (0..second_array.count).map do |n|
+		 next if second_array[n].nil?
 		 if n == 0
-			res = compare_elements(old_hash, new_array[0])
+			res = compare_elements(first_hash, second_array[0])
 			result[:update][n] = res unless res.empty?
 		 else
-			result[:append][n] = new_array[n]
+			result[:append][n] = second_array[n]
 		 end
 	  end
 
 	  filter_results(result)
    end
 
-   def compare_strings(old_string, new_string)
-	  (old_string != new_string) ? new_string.to_s : ""
+   def compare_strings(first_string, second_string)
+	  (first_string != second_string) ? second_string.to_s : ""
    end
 
    # Returns diffs-hash with bare structure
@@ -116,9 +132,10 @@ class JsonDiffer
 	  out_result
    end
 
-   def self.get_diff(old, new, exclusion = [])
+   def self.get_diff(first, second, exclusion = [])
 	  comparer = JsonDiffer.new
 	  comparer.excluded_keys = exclusion
-	  comparer.compare_elements(old, new)
+	  comparer.compare_elements(first, second)
    end
+
 end
